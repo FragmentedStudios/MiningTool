411cfd8c102bd3692fb71c8b0e12629306212e84
Tom Hauser <tom.hauser@gmail.com>
2011-12-15 23:07:49 -0500

    fixing all the checks

--- includes/igb.php
+++ includes/igb.php
@@ -21,12 +21,12 @@ class IGB
 {
 	
 	//Define global vars to be set by the functions
-	var $no_igb;
+	var $no_igb, $trusted;
 		
 	function IGB()
 	{
 		//Define the IGB indentifying header
-		$trusted = $_SERVER['HTTP_EVE_TRUSTED'];
+		$this->trusted = $_SERVER['HTTP_EVE_TRUSTED'];
 		
 		//Check the user's settings to see if NoIGB logins are allowed
 		include('config.php');
@@ -34,50 +34,28 @@ class IGB
 	}
 	
 	//Check to see if IGB headers are present
-	function checkIGB($no_igb)
+	function checkIGB()
 	{
 		//Check to see if IGB is required for access
-		if ($no_igb == 0)
+		if ($this->no_igb == 0)
 		{
-			//If IGB is required, check if present
-			if (!$trusted)
-			{
-				//If no IGB headers present, return an error
-				return 0;
-			}
-			else if ($trusted == 0)
-			{
-				//If IGB is present, but site is not trustd, return an error
-				return 1;
-			}
-			else if ($trusted == 1)
-			{
-				//If IGB is present and trusted, return no errors and allow user to continue
-				return 2;
-			}
-			else
-			{
-				//If IGB is present, but is an invalid value return an error
-				return 3;
+			
+			switch ($this->trusted){
+				case "":
+					return 0; // doesn't exist. says so in browser
+				case "No":
+					return 1; // igb used, but not trusted.
+				case "Yes":
+					return 2; // igb used and trusted
 			}
 		}
-		else
+		else // the in game browser isnt' required. 
 		{
-			//Check to see if IGB is present
-			if (!trusted)
-			{
-				//If IGB is not present, don't look for header info but let user pass
-				return 0;
-			}
-			else if ($trusted == 1)
-			{
-				//If IGB is present and trusted, allow the user to pass and gather header info
-				return 1;
-			}
-			else
-			{
-				//If IGB is present, but status unknown let the user pass but don't 
-				return 0;
+			switch ($this->trusted){
+				case "Yes":
+					return 2; // they are using igb and have headers for us to gather
+				default: // anything other than a 'yes' in the trusted field returns 0
+					return 0;
 			}
 		}
 	}
@@ -85,7 +63,6 @@ class IGB
 	//Gather IGB information on the connected user
 	function getIGBInfo()
 	{
-		
 		//Gather the IGB Headers for the user
 		$character = $_SERVER['HTTP_EVE_CHARNAME'];
 		$regionid = $_SERVER['HTTP_EVE_REGIONID'];
@@ -102,14 +79,13 @@ class IGB
 								'solar' => '$solarid',
 								'ship' => '$shipid',
 								'corp' => '$corp',
-								'corpr' => '$corprole');
-								
+								'corpr' => '$corprole');								
 		//Return the array as the value of the function
 		return $igb_headers;
 	}
 }
 
 //Activate the class
-$IGB = new IGB;
+$igb = new IGB;
 
 ?>	
