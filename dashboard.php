7298188d8b8ffb31c76cd59fd90017e1537dac81
Tom Hauser <tom.hauser@gmail.com>
2011-12-15 19:01:30 -0500

    sketchy initial html. by no means final or how it is supposed to look at all

--- dashboard.php
+++ dashboard.php
@@ -31,42 +31,29 @@ if ($igb->checkIGB == 1)
 //Start the output of the page
 
 ?>
-// javascript includes
 <html>
 <head>
-	<script type="text/javascript" src="minerScripts.js"></script>
-	<script type="text/javascript" src="haulerScripts.js"></script>
-	<script type="text/javascript" src="operationScripts.js"></script>
+<script type="text/javascript" src="js/dashboard.js"></script>
+<script type="text/javascript">
+function startTime(){
+	var a = setTimeout("getCurrentOps()",3000);
+	var b = setTimeout("getFutureOps()",3000);
+	var c = setTimeout("getOnlineUsers()",3000);
+	var d = setTimeout("getLifetimeOre()",3000);	
+}
+
+</script>
 </head>
-
-
-<body>
-//The dropdown that will be populated by the server, ore types
-<form id="oreSelector">
-	<select name="oreTypes" id="oreTypes">
-		<option value="1">Veldspar</option>
-		<option value="2">Scordite</option>
-		<option value="3">Pyroxeres</option>
-		<option value="4">Plagioclase</option>
-		<option value="5">Omber</option>
-		<option value="6">Kernite</option>
-		<option value="7">Jaspet</option>
-		<option value="8">Hemorphite</option>
-		<option value="9">Hedbergite</option>
-		<option value="10">Gneiss</option>
-		<option value="11">Dark Ochre</option>
-		<option value="12">Crokite</option>
-		<option value="13">Spodumain</option>
-		<option value="14">Bistot</option>
-		<option value="15">Arkonor</option>
-		<option value="16">Mercoxit</option>
-	</select>
-	<input type="text" style="text-align: right" value="Enter yield here" name="yield"/>
-	<input type="button" onClick="addOre(oreTypes.value, yield.value)" value="Submit Transaction" name="submit"/>
-</form>
-<br />
-
-<div id="transactionTable"><b>Transaction information goes here.</b></div>
+<body onload="startTimer()">
+	<!-- table containing current operations -->
+	<h2>Current Operations</h2>
+	<div id="currentOpSchedule"><b>Current Operations are listed here...</b></div>
+	<h2>Future Operations</h2>
+	<div id="futureOpsSchedule"><b>Future Operations are listed here...</b></div>
+	<h2>Online Users</h2>
+	<div id="onlineUserList"><b>The list of online users is here...</b></div>
+	<h2>Lifetime Ore Mined</h2>
+	<div id="lifetimeOreAmount"><b>1000000</b></div>
 </body>
 
 </html>
